d919a45386904ce345739d8379b3408eb20c1573
Rushikesh Chougule <rushikeshchougule3299@gmail.com>
2024-01-03 16:48:25 +0900

    load a 3d model

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
 const App = () => {
+  const computerModel = useGLTF("/desktop_pc/scene.gltf");
   return (
     <>
-      <Canvas camera={{ position: [25, 0, 25], fov: 40 }}>
+      <Canvas camera={{ position: [30, 0, 25], fov: 20 }}>
         <ambientLight intensity={1} />
         {/* <hemisphereLight args={[0xffffbb, 0x080820, 1]} />/ */}
         {/* <directionalLight color="red" position={[0, 5, 5]} /> */}
         <mesh>
-          <sphereGeometry args={[, 64, 32]} />
-          <meshStandardMaterial color={"red"} roughness={0} />
+          <primitive object={computerModel.scene} />
+          {/* <sphereGeometry args={[, 64, 32]} />
+          <meshStandardMaterial color={"red"} roughness={0} /> */}
         </mesh>
         <OrbitControls />
       </Canvas>
